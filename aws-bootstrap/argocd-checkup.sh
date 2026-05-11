0cffa1057df09795642705fe06420d7e6f10ddf3
Paul123111 <20102629wit@gmail.com>
2026-05-11 14:35:08 +0100

    feat: finish infra and worker setup script

--- aws-bootstrap/argocd-checkup.sh
+++ aws-bootstrap/argocd-checkup.sh
@@ -2,7 +2,10 @@
 
 # just for testing at the moment, might be removed later
 
+sudo apt-get install -y socat
+
 sudo kubectl port-forward svc/argocd-server -n argocd 8080:443 > /dev/null 2>&1 &
+sudo kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
 
 ARGOCD_PWD=$(sudo kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
 argocd login localhost:8080 --username admin --password "$ARGOCD_PWD" --insecure
