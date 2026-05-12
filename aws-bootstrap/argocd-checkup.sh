2315cddae63511c3ee5b286b2ac4b87f32f0077e
Paul123111 <20102629wit@gmail.com>
2026-05-12 11:43:51 +0100

    feat: Add ingress

--- aws-bootstrap/argocd-checkup.sh
+++ aws-bootstrap/argocd-checkup.sh
@@ -15,3 +15,8 @@ ssh -i .\.ssh\planet-hunter-key.pem -L 8080:localhost:8080 ubuntu@111.111.111.11
 ssh -i .\.ssh\planet-hunter-key.pem ubuntu@111.111.111.111
 
 sudo kubectl port-forward --address 0.0.0.0 svc/argocd-server -n argocd 8080:443
+
+aws ssm get-parameter --name "/k3s/cloudflare-token" \
+  --with-decryption --query "Parameter.Value" \
+  --output text | tr -d '[:space:]' | \
+  xargs -I {} sudo kubectl create secret generic cloudflare-api-token-secret -n cert-manager --from-literal=api-token={}
